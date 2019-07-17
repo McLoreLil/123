ac26800a8ae895101a9b33dad46088598b78400a
Kory McGregor <artemka076.dimaneal@gmail.com>
2019-07-18 01:51:31 +0300

    test

---
+++ index.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+const Trello = require("trello");
+
+const bot = new Discord.Client();
+const trello = new Trello(process.env.application_key, process.env.user_token);
+
+const allow_servers = [
+    '543799835652915241' // Спец.Администрация Arizona Games;
+];
+
+const allow_users = {
+    "336207279412215809": {
+        "add-cards": true
+    }
+};
+
+bot.login(process.env.token);
+
+bot.on('ready', () => {
+    console.log(`Бот был успешно запущен!`);
+});
+
+bot.on('message', async (message) => {
+    if (message.channel.type == "dm") return
+    if (message.author.bot) return
+    if (!allow_servers.some(server => message.guild.id == server)) return
+    let user = allow_users["336207279412215809"]
+    if (!user) return
+    
+    if (message.content.startsWith('/bug')){
+        if (!user["add-cards"]) return message.reply(`недостаточно прав доступа!`);
+        const description = message.content.split('/bug ')[1];
+        trello.addCard(`Bug Report #${new Date().valueOf()}`, `${description}`, `5d2c6bc16cfdd530bb00d786`, (error, trelloCard) => {
+            if (error) return message.reply(`произошла ошибка при добавлении отчёта в баг-трекер.`);
+            message.reply(`вы отправили отчёт об ошибке в баг-трекер.`);
+        });
+    }
+});
