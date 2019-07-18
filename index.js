4b6e95175dd344a8bbb352ceea33efba969d3de3
Kory McGregor <artemka076.dimaneal@gmail.com>
2019-07-18 03:33:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ const allow_servers = [
 
 const allow_users = {
     "336207279412215809": {
-        "add-cards": true
+        "add-cards": true,
+        "upload_images": true,
     }
 };
 
@@ -40,19 +41,33 @@ bot.on('message', async (message) => {
     if (!user) return
     
     if (message.content.startsWith('/bug')){
+        message.delete();
         if (!user["add-cards"]) return message.reply(`недостаточно прав доступа!`);
         const description = message.content.split('/bug ')[1];
         if (!description) return message.reply(`введите описание ошибки. ошибка будет передана разработчикам arizona rp`);
         if (description.length < 5) return message.reply(`описание должно быть ясным и понятным для его отправки`);
         server.query(`SELECT \`AUTO_INCREMENT\` FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'trello'`, (err, answer) => {
             if (err) return message.reply(`произошла ошибка базы данных, повторите попытку позже.`);
-            trello.addCardWithExtraParams(`Баг-репорт №${+answer[0]["AUTO_INCREMENT"]}`, { desc: `${description}`, idLabels: ['5cbc573c91d0c2ddc5a8f953'] }, '5cbc574a34ba2e8701f64359', (error, trelloCard) => {
+            trello.addCardWithExtraParams(`Баг-репорт №${+answer[0]["AUTO_INCREMENT"]}`, { desc: `Discord отправителя: ${message.member.displayName || message.member.user.tag} [${message.author.id}]\nСуть недоработки: ${description}`, idLabels: ['5cbc573c91d0c2ddc5a8f953'] }, '5cbc574a34ba2e8701f64359', (error, trelloCard) => {
                 if (error) return message.reply(`произошла ошибка при добавлении отчёта в баг-трекер.`);
                 server.query(`INSERT INTO \`trello\` (\`card\`, \`author\`, \`description\`) VALUES ('${trelloCard.id}', '${message.author.id}', '${description}')`, (error) => {
                     if (error) return message.reply(`произошла ошибка запроса к базе данных, повторите попытку позже.`);
                     message.reply(`вы отправили отчёт об ошибке №${+answer[0]["AUTO_INCREMENT"]} в баг-трекер.`);
-                    trello.addLabelToCard(`${trelloCard.id}`, ``)
                 });
+            });
+        });
+    }
+
+    if (message.content.startsWith('/add')){
+        message.delete();
+        if (!user["upload-images"]) return message.reply(`недостаточно прав доступа!`);
+        const args = message.content.slice('/add').split(/ +/);
+        if (!args[1] || !args[2]) return message.reply(`укажите номер ошибки и ссылку. /add [номер] [url]`);
+        server.query(`SELECT * FROM \`trello\` WHERE \`id\` = '${args[1]}'`, (error, answer) => {
+            if (error) return message.reply(`произошла ошибка базы данных, сообщите администратору.`);
+            trello.addAttachmentToCard(`${answer[0].card}`, `${args[2]}`, (error) => {
+                if (error) return message.reply(`произошла ошибка при добавлении доказательств.`);
+                message.reply(`вы успешно прикрепили доказательства к карточке №${args[1]} в баг-трекере.`);
             });
         });
     }
